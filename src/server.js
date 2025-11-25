7e470e40c39c00c02549a5582151322470cb17c0
sudhan_1999 <jsudhan68@gmail.com>
2025-11-25 12:12:34 +0530

    index changed to app

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import app from "./index.js";
+import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
 const PORT = process.env.PORT || 3000;
